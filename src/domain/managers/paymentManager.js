5b38521d9793c1d813db74a9f709344529a45ddd
gganud <gastonanud@gmail.com>
2023-09-17 22:51:51 -0300

    Refactor

--- src/domain/managers/paymentManager.js
+++ src/domain/managers/paymentManager.js
@@ -1,22 +1,31 @@
 import Stripe from 'stripe';
 import config from '../../config/index.js';
 import TicketManager from './ticketManager.js';
+import ProductManager from './productManager.js';
 
 class PaymentManager
 {
   constructor()
   {
     this.ticketManager = new TicketManager();
+    this.productManager = new ProductManager();
     this.stripe = new Stripe(config.stripeKey);
   }
   async payTicket({ id, amount, currency = 'usd', purchaser })
   {
-    const ticket = this.ticketManager.getOne(id);
+    const ticket = await this.ticketManager.getOne(id);
     if (ticket.orderCompleted === true)
     {
       throw new Error('Order is already paid');
     }
-
+    for (const product of ticket.products)
+    {
+      const productInDb = await this.productManager.getProductById(product.id);
+      if (product.quantity > productInDb.quantity)
+      {
+        throw new Error('No stock available.');
+      }
+    }
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       currency,
